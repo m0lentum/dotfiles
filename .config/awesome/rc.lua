9e99bbc67318c503504a2133cb293e0a99f99ea7
Mikael Myyrä <mikael.myyrae@gmail.com>
2020-03-15 19:14:52 +0200

    awesome: Remove autostarts
    None of that stuff was actually used or even worked

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -59,30 +59,6 @@ do
 end
 -- }}}
 
--- {{{ Autostart windowless processes
-
--- This function will run once every time Awesome is started
-local function run_once(cmd_arr)
-    for _, cmd in ipairs(cmd_arr) do
-        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
-    end
-end
-
-run_once({"urxvtd", "unclutter -root"}) -- entries must be separated by commas
--- custom: run stretchly
-run_once({"stretchly"})
-
--- This function implements the XDG autostart specification
---[[
-awful.spawn.with_shell(
-    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
-    'xrdb -merge <<< "awesome.started:true";' ..
-    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment Awesome --autostart --search-paths "$XDG_CONFIG_DIRS/autostart:$XDG_CONFIG_HOME/autostart"' -- https://github.com/jceb/dex
-)
---]]
--- }}}
-
 -- {{{ Variable definitions
 -- global so they can be used in keybinds.lua
 modkey = "Mod4"
