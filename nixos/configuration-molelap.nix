6f86ad573d51a82a9bac428d41374eff9741fc0f
Mikael Myyrä <mikael.myyrae@gmail.com>
2020-03-15 14:17:26 +0200

    Set Awesome gaps to 0 on molelap

--- nixos/configuration-molelap.nix
+++ nixos/configuration-molelap.nix
@@ -8,6 +8,8 @@
   # Enable touchpad support.
   services.xserver.libinput.enable = true;
 
+  environment.variables.AWESOME_GAP = "0";
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
