a2f4bc4ef387729c6362596f33e04b3da4cbc05f
Mikael Myyrä <mikael.myyrae@gmail.com>
2020-05-06 00:00:35 +0300

    Add a couple of global scripts using writeScriptBin

--- home-manager/home-moletable.nix
+++ home-manager/home-moletable.nix
@@ -1,5 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  csp = pkgs.writeScriptBin "csp" ''
+    #!${pkgs.stdenv.shell}
+    wine "/home/mole/.wine/drive_c/Program Files/CELSYS/CLIP STUDIO 1.5/CLIP STUDIO PAINT/CLIPStudioPaint.exe"
+  '';
+  cam = pkgs.writeScriptBin "cam" ''
+    #!${pkgs.stdenv.shell}
+    PAD_IP="192.168.0.189"
+    PHONE_IP="TODO"
+    if [[ $1 = "phone" ]]; then IP="$PHONE_IP"; else IP="$PAD_IP"; fi
+
+    ffmpeg -f mjpeg -i "http://$IP/live" -pix_fmt yuv420p -f v4l2 /dev/video0
+  '';
+in
 { imports = [ ./common.nix ];
   nixpkgs.config.allowUnfree = true;
   programs = {
@@ -14,6 +28,9 @@
     };
   };
   home.packages = with pkgs; [
+    csp
+    cam
+    ffmpeg
     discord
     spotify
     steam
