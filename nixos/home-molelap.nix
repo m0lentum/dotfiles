e6399cee9f357bf0b955e97d7c90498f3233f7de
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-01-07 15:34:16 +0200

    molelap: Disable picom

--- nixos/home-molelap.nix
+++ nixos/home-molelap.nix
@@ -1,4 +1,7 @@
 { config, pkgs, ... }:
 
-{ imports = [ ./home-common.nix ];
+{
+  imports = [ ./home-common.nix ];
+  # picom causes too much latency on this slow computer, turn it off
+  services.picom.enable = pkgs.lib.mkForce false;
 }
