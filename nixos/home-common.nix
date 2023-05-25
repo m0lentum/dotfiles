36d151e4219c547ab546f36a2dc7047b3d1edb2d
Mikael Myyrä <mikael.myyrae@gmail.com>
2023-05-25 17:47:52 +0300

    experiment: Switch to use flakes instead of channels
    Not fully sure about this, it's somewhat slower
    to `switch` than home-manager which can be annoying.
    Will try this for a while and possibly switch back

--- nixos/home-common.nix
+++ nixos/home-common.nix
@@ -1,10 +1,6 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgsUnstable, ... }:
 
 let
-  # generally avoiding unstable but using it to keep up with some
-  # particularly fast-moving programs like nushell
-  pkgsUnstable = import <nixos-unstable> { };
-
   # directories to ignore in tree and fzf listings because they're
   # never what I'm looking for and make lists too big to navigate
   listIgnores = [
@@ -58,7 +54,6 @@ in
     lsd.enable = true;
     feh.enable = true;
     direnv.enable = true;
-    home-manager.enable = true;
     nix-index.enable = true;
   };
 
