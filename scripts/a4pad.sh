8b1b6aca424dcd69c8dd97551b56c3a00e95ffea
Mikael Myyrä <mikael.myyrae@gmail.com>
2025-03-06 22:34:47 +0200

    Fix format errors from converting a4pad.sh from nix to sh

--- scripts/a4pad.sh
+++ scripts/a4pad.sh
@@ -19,28 +19,28 @@ h=$(echo $dims | rg '^[0-9]+\s?x\s?([0-9]+)$' -r '$1')
 if [ $h -gt $w ]; then
     echo "portrait"
     aspect_error=$(echo "($h/$w)-$A4_ASPECT" | bc -l)
-    if [ "''${aspect_error:0:1}" != "-" ]; then
+    if [ "${aspect_error:0:1}" != "-" ]; then
 	target_h=$h
 	target_w=$(echo "$h/$A4_ASPECT" | bc -l | xargs printf "%.0f")
-	echo "padding horizontally to ''${target_w}x''${target_h}"
+	echo "padding horizontally to ${target_w}x${target_h}"
     else
 	target_w=$w
 	target_h=$(echo "$w*$A4_ASPECT" | bc -l | xargs printf "%.0f")
-	echo "padding vertically to ''${target_w}x''${target_h}"
+	echo "padding vertically to ${target_w}x${target_h}"
     fi
 else
     echo "landscape"
     aspect_error=$(echo "($w/$h)-$A4_ASPECT" | bc -l)
-    if [ "''${aspect_error:0:1}" != "-" ]; then
+    if [ "${aspect_error:0:1}" != "-" ]; then
 	target_w=$w
 	target_h=$(echo "$w/$A4_ASPECT" | bc -l | xargs printf "%.0f")
-	echo "padding vertically to ''${target_w}x''${target_h}"
+	echo "padding vertically to ${target_w}x${target_h}"
     else
 	target_h=$h
 	target_w=$(echo "$h*$A4_ASPECT" | bc -l | xargs printf "%.0f")
-	echo "padding horizontally to ''${target_w}x''${target_h}"
+	echo "padding horizontally to ${target_w}x${target_h}"
     fi
 fi
 
-dims="''${target_w}x''${target_h}"
-convert "$src" -resize "$dims" -background white -gravity northwest -extent "$dims" "$dest"
+dims="${target_w}x${target_h}"
+magick "$src" -resize "$dims" -background white -gravity northwest -extent "$dims" "$dest"
